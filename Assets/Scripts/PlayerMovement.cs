ce61b9871de5eb43097cc9847657b0439fa7ad10
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-07-26 18:29:29 +0530

    Added PlayerMovement.cs
    Added some functionalities using the new unity input system.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    
+
+    Vector2 moveInputs;
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInputs = value.Get<Vector2>();
+
+        Debug.Log(moveInputs); 
+    }
+    
+    void OnJump(InputValue value)
+    {
+        myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+    }
+
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInputs.x * runSpeed, myRigidbody.velocity.y); 
+        myRigidbody.velocity = playerVelocity;
+
+        bool playerIsRunningTransition = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        myAnimator.SetBool("isRunning", playerIsRunningTransition);
+    }
+
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+
+
+
+
+
+
+}
