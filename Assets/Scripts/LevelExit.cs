38ca2857623685aa59c1a6f69d294242f5e04001
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2022-07-30 20:47:10 +0530

    LevelExit.cs Added

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float LevelLoadDelay = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+        
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log("Game Over");
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+
+    }
+
+   
+}
